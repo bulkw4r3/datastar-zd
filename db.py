70c3b4478d74293bbcf3b37691874aa5287f9084
Eddy Shoemaker <bulkware@posteo.org>
2026-06-14 21:42:12 +0200

    Update db.py

--- db.py
+++ db.py
@@ -6,6 +6,7 @@ LOCAL_DB_PATH = "datastar.db"
 def get_connection():
     conn = sqlite3.connect(LOCAL_DB_PATH)
     conn.execute("PRAGMA journal_mode=WAL")
+    conn.execute("PRAGMA synchronous=NORMAL")
     return conn
 
 
